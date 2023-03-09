84f90d3631603d8fef46b393d8f09cd3af0adb6e
Anna Zhezhera <ianna22121996@gmail.com>
2023-03-09 17:54:33 +0200

    initial

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import React, { lazy } from 'react';
+import { Layout } from './Layout/Layout';
+import { AppWrapp } from './App.styled';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const HeroDetails = lazy(() => import('pages/HeroDetails'));
+
+export const App = () => {
+  return (
+    <AppWrapp>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="character/:id" element={<HeroDetails />} />
+          <Route path="*" element={<Home />} /> setSearchQuery(movieName);
+        </Route>
+      </Routes>
+    </AppWrapp>
+  );
+};
