ba6b02aede53cdc76b90a2faa0cfc74e1700b40b
AnnaZhezhera <101525831+AnnaZhezhera@users.noreply.github.com>
2023-03-17 11:06:49 +0000

    Deploying to gh-pages from @ AnnaZhezhera/get-your-rick@f9017db39d546e5ad8ea7cc34cf697073fe16731 🚀

---
+++ static/js/830.2eefd6e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkget_your_rick=self.webpackChunkget_your_rick||[]).push([[830],{830:function(e,n,t){t.r(n);var r=t(184);n.default=function(){return(0,r.jsx)("div",{style:{textAlign:"center",padding:30},children:(0,r.jsx)("h2",{children:"404 Page not found"})})}}}]);
+//# sourceMappingURL=830.2eefd6e9.chunk.js.map
